b81458f824b58b5a096b10822e0644670dc80f9a
sanikamamidwar08 <sanikamamidwar4@gmail.com>
2024-09-25 21:34:05 -0700

    if elif else statment

---
+++ ifstatment.py
@@ -0,0 +1,10 @@
+n1=int(input("Enter no 1:"))
+n2=int(input("Enter no 2:"))
+n3=int(input("Enter no 3:"))
+if n1>n2 and n1>n3:
+    print (n1,"is greater....!")
+elif n2>n1 and n2>n3:
+    print(n2,"is greater.....!")
+else:
+    print(n3,"is greater.....!")
+    
